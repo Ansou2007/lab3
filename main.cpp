b5f664b8ed2afa1f6b08359ac2bc3c5d6f7b46ab
Ansoumane Michel <ansoumanemichel.tamba@unchk.edu.sn>
2023-06-11 22:05:04 +0000

    premier commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a = 55;
+    int b = 5;
+
+    cout << "La somme de a + b donne: " << a + b << endl;
+    cout << "Le produit de a x b donne : " << a * b << endl;
+    cout << "Le quotient de a : b donne : " << a / b << endl;
+    cout << "Le reste du quotient donne : " << a % b << endl;
+    return 0;
+}
