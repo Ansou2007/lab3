94cc65e4c93a0f3ebc0c581dbff79de3eb804271
Ansoumane Michel <ansoumanemichel.tamba@unchk.edu.sn>
2023-06-14 17:28:29 +0000

    modification branch tamba

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main()
 {
 
-    int a = 55;
+    //int a = 55;
     int b = 5;
-
+    cout << "Mettre le premier nombre" << endl;
+    cin >> a;
     cout << "La somme de a + b donne: " << a + b << endl;
     cout << "Le produit de a x b donne : " << a * b << endl;
     cout << "Le quotient de a : b donne : " << a / b << endl;
